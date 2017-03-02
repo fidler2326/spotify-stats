2a0d2adf7285cc5f314adb79c92ad55f2465f225
Adam <fidler2326@gmail.com>
2017-03-02 18:50:32 +0000

    Update url

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var port = process.env.PORT || 5000;
 var client_id = 'acd2290b94ae48eca2d7a1f638bd3623'; // Your client id
 var client_secret = '7693a353c8d4465c9a0b6dfd48d41d4c'; // Your secret
 // var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
-var redirect_uri = 'http://spotify-stats.herokuapp.com/callback/'; // Your redirect uri
+var redirect_uri = 'http://spotify.reldif.co.uk/callback/'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
